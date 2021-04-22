71549887fe7898ca8874ce0b9952e729adbbacf8
alex olivares <alexander.b.olivares@gmail.com>
2021-04-22 16:40:09 -0500

    hashrouter

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./About";
 import Contact from "./Contact";
 import Projects from "./Projects";
